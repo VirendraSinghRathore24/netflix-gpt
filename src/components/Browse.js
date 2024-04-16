cc3f2119a7c2acb563d020be37ead0c0fc4a51a1
VirendraSinghRathore24 <virendrasinghrathore24@gmail.com>
2024-04-17 00:28:57 +0530

    Gpt

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,23 +1,33 @@
 import React, { useEffect } from 'react'
 import Header from './Header'
 import { API_OPTIONS } from '../utils/contants'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { addNowPlayingMovies } from '../utils/movieSlice'
 import useNowPlayingMovies from '../hooks/useNowPlayingMovies'
 import MainContainer from './MainContainer'
 import SecondaryContainer from './SecondaryContainer'
 import usePopularMovies from '../hooks/usePopularMovies'
+import GptSearch from './GptSearch'
 
 const Browse = () => {
 
+    const showGptSearch = useSelector(store => store.gpt.showGptSearch);
     const nowPlayingMovies = useNowPlayingMovies();
     usePopularMovies();
 
   return (
     <div>
         <Header/>
-        <MainContainer/>
+        {
+            showGptSearch ? <GptSearch/> : 
+                <div>
+                <MainContainer/>
         <SecondaryContainer/>
+                </div>
+            
+        }
+        
+        
     </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,15 @@ import { auth } from '../utils/firebase'
 import { useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { adduser, removeUser } from '../utils/userSlice'
-import { LOGO } from '../utils/contants'
+import { LOGO, SUPPORTED_LANGUAGES } from '../utils/contants'
+import { toggleGptSearchView } from '../utils/gptSlice'
+import { changeLanguage } from '../utils/configSlice'
 
 
 const Header = () => {
     const user = useSelector(store => store.user);
+    const showGptSearch = useSelector(store => store.gpt.showGptSearch);
+
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const handleSignOut =  () => {
@@ -17,6 +21,13 @@ const Header = () => {
         }).catch((error) => {
 
         })
+    }
+    const handleGptSearch = () => {
+        dispatch(toggleGptSearchView())
+    }
+
+    const handleLangChange = (e) => {
+        dispatch(changeLanguage(e.target.value))
     }
 
     useEffect(() => {
@@ -42,9 +53,18 @@ const Header = () => {
 
     <img className='w-44' src={LOGO} alt='logo'  />
     {
-        user && (<div className='flex '>
-    <h1>{user?.displayName}</h1>
-        <button onClick={handleSignOut} className='px-4 py-1 text-lg '>Sign Out</button>
+        user && (<div className='flex justify-center items-center gap-x-4 text-lg'>
+        { showGptSearch && 
+            <select className='p-2 bg-gray-900 text-white m-2' onChange={handleLangChange}>
+            {
+                SUPPORTED_LANGUAGES.map(lang => <option key={lang.identifier} value={lang.identifier}>{lang.name}</option>)
+            }
+            </select>
+        }
+        <button onClick={handleGptSearch} className='text-white px-4 py-1 bg-purple-800 rounded-lg'>
+        {showGptSearch ? "Home" : "GPT Search"}</button>
+        <h1 className='text-white'>{user?.displayName}</h1>
+        <button onClick={handleSignOut} className='px-4  text-white'>Sign Out</button>
     </div>) 
     }
         </div>
